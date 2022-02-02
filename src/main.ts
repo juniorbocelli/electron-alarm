f113b7004cb6762ea8f467e15e208d69baa2333f
Júnior Bocelli <juniorbocelli@gmail.com>
2022-02-02 11:33:46 -0300

    Add disable GPU aceleration.

--- src/main.ts
+++ src/main.ts
@@ -10,37 +10,58 @@ const createWindow = (): void => {
     }
   });
 
-  console.log('isDev', isDev);
-
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:9000'
       : `file://${app.getAppPath()}/index.html`,
   );
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 };
+
+app.disableHardwareAcceleration();
+
+// const isRelaunch = (): boolean => {
+//   // Check if is relaunch
+//   return app.commandLine.hasSwitch('relaunch');
+// };
+
+// // Check if disable GPU
+// if (app.commandLine.hasSwitch('disable-gpu'))
+//   app.disableHardwareAcceleration();
+
+// app.on('child-process-gone', (event, killed) => {
+//   var opt: Electron.RelaunchOptions = {};
+//   opt.args = process.argv.slice(1).concat(['--relaunch']);
+//   opt.execPath = process.execPath;
+
+//   if (killed.type === 'GPU') {
+//     console.error('ERROR -- GPU');
+//     if (isRelaunch())
+//       app.quit();
+
+//     app.exit(0);
+//     app.relaunch(opt);
+//   };
+// });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
